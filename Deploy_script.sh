59df734a14c16f628b81f745ee1925b96f335c47
sagarjr11 <165011272+sagarjr11@users.noreply.github.com>
2025-07-26 10:42:42 +0530

    Update Deploy_script.sh

--- Deploy_script.sh
+++ Deploy_script.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-echo "Simulating deployment to staging..."
-docker run -d -p 3000:3000 yourdockerhubusername/ci-cd-nodejs-app:latest
+
+set -e                           # Exit immediately if a command exits with a non-zero status
+
+echo " ------------------Deployment to staging...---------------------------------"
+
+# Stop existing container if running
+
+EXISTING_CONTAINER=$(docker ps -q --filter "ancestor=yourdockerhubusername/ci-cd-nodejs-app:latest")
+if [ -n "$EXISTING_CONTAINER" ]; then
+    echo "Stopping existing container..."
+    docker stop $EXISTING_CONTAINER
+fi
+
+# Run the new container
+docker run -d --rm -p 3000:3000 --name nodejs-app yourdockerhubusername/ci-cd-nodejs-app:latest
+
+echo "App deployed and running at http://localhost:3000"
